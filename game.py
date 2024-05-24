662a7f00f938ef65ff59962b62af9d175c19fe94
Jacob-Norman <157053783+Jacob-Norman@users.noreply.github.com>
2024-05-24 13:12:26 -0230

    Add files via upload

---
+++ game.py
@@ -0,0 +1,18 @@
+from board import drawEmptyBoard
+from time import sleep
+
+# If this function is exited without the program terminating then the player is ready to play
+def startMenu():
+    response = ''
+    while response != "y" or response != "q":
+        drawEmptyBoard()
+        response = input("Please adjust your terminal size so that the entire board is visible.\nIf you are ready to play, type 'y' if you would like to quit type 'q': ")
+        response.lower()
+        if response == "y":
+            break
+        elif response == "q":
+            exit()
+        else:
+            print("Please enter either 'y' or 'q'")
+            sleep(1.5)
+    return 0
